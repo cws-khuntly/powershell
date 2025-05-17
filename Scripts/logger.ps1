f4bc346e3833fe3889a364e4f8853f54308d7971
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2025-05-17 09:38:26 -0400

    Update logger.ps1

--- Scripts/logger.ps1
+++ Scripts/logger.ps1
@@ -4,15 +4,28 @@ if (Test-Path -Path "${LoggingPropertyFile}" -Type Leaf) {
     $LoggingProperties = Read-Properties "${LoggingPropertyFile}"
 
     if ([string]::IsNullOrEmpty("${LoggingProperties}")) {
-        $LOG_ROOT = "${LoggingProperties}["LOG_ROOT"]"
-        $ENABLE_DEBUG = "${LoggingProperties}["ENABLE_DEBUG"]"
-        $ENABLE_VERBOSE = "${LoggingProperties}["ENABLE_VERBOSE"]"
-        $ENABLE_TRACE = "${LoggingProperties}["ENABLE_TRACE"]"
-        $ENABLE_PERFORMANCE = "${LoggingProperties}["ENABLE_PERFORMANCE"]"
+        $LogRoot = "${LoggingProperties}["LOG_ROOT"]"
 
-if (!(Test-Path -Path "${LOG_ROOT}" -PathType Container)) { New-Item -ItemType Directory -Path "${LOG_ROOT}" }
+        if (!(Test-Path -Path "${LOG_ROOT}" -PathType Container)) { New-Item -ItemType Directory -Path "${LOG_ROOT}" }
 
-function writeLogEntry {
+        setLoggingSession
+    } else {
+        Write-Error -Message "Failed to load logging configuration. No logging available!"
+    }
+} else {
+    Write-Error -Message "Failed to load logging configuration. No logging available!"
+}
+
+Function setLoggingSession() {
+    Set-Variable -Name "LoggingLoaded" -Value $true -Scope Global
+
+    Set-Variable -Name "IsDebugEnabled" -Value "${LoggingProperties}["ENABLE_DEBUG"]" -Scope Session
+    Set-Variable -Name "IsVerboseEnabled" -Value "${LoggingProperties}["ENABLE_VERBOSE"]" -Scope Session
+    Set-Variable -Name "IsTraceEnabled" -Value "${LoggingProperties}["ENABLE_TRACE"]" -Scope Session
+    Set-Variable -Name "IsPerfEnabled" -Value "${LoggingProperties}["ENABLE_PERFORMANCE"]" -Scope Session
+}
+
+Function writeLogEntry() {
     param (
         [Parameter(Mandatory=$true)]
         [string] $LogType,
@@ -26,18 +39,13 @@ function writeLogEntry {
         [string] $LogMessage
     )
 
-    Switch -Regex (${LogType}) {
-        "[Cc][Oo][Nn][SS][Oo][Ll][Ee]" { do things }
-        "[Ff][Ii][Ll][Ee]" { dothings }
-        default { }
+    Switch -Regex ("${LogType}") {
+        "[Cc][Oo][Nn][SS][Oo][Ll][Ee]" { writeLogEntryToConsole "${LogLevel}" "${LogMessage}" }
+        "[Ff][Ii][Ll][Ee]" { writeLogEntryToFile "${LogType}", "${LogLevel}", "${ProcessID}", "${ClassName}", "${LineNumber}", "${FunctionName}", "${LogMessage}" }
     }
-    $Timestamp = Get-Date -Format "${TIMESTAMP_OPTS}"
-    [string]::Format("${CONVERSION_PATTERN}",$name)
-    $LogEntry = "$Timestamp - $Message"
-    Add-Content -Path $LogPath -Value $LogEntry
 }
 
-Function writeLogEntryToConsole {
+Function writeLogEntryToConsole() {
     param (
         [Parameter(Mandatory=$true)]
         [string] $LogLevel,
@@ -51,42 +59,39 @@ Function writeLogEntryToConsole {
     }
 }
 
-Function writeLogEntryToFile {
+Function writeLogEntryToFile() {
     param (
         [Parameter(Mandatory=$true)]
         [string] $LogType,
         [Parameter(Mandatory=$true)]
         [string] $LogLevel,
+        [Parameter(Mandatory=$true)]
         [int] $ProcessID,
+        [Parameter(Mandatory=$true)]
         [string] $ClassName,
+        [Parameter(Mandatory=$true)]
         [int] $LineNumber,
+        [Parameter(Mandatory=$true)]
         [string] $FunctionName,
         [Parameter(Mandatory=$true)]
         [string] $LogMessage
     )
 
     Switch -Regex (${LogLevel}) {
-        "[Cc][Oo][Nn][SS][Oo][Ll][Ee]" { do things }
-        "[Ff][Ii][Ll][Ee]" { dothings }
-        default { }
+        "[Pp][Ee][Rr][Ff][Oo][Rr][Mm][Aa][Nn][Cc][Ee]|[Pp][Ee][Rr][Ff]" { $LogFile = "${LoggingProperties}["PERF_LOG_FILE"]" }
+        "[Ff][Aa][Tt][Aa][Ll]" { $LogFile = "${LoggingProperties}["ERROR_LOG_FILE"]" }
+        "[Ee][Rr][Rr][Oo][Rr]" { $LogFile = "${LoggingProperties}["ERROR_LOG_FILE"]" }
+        "[Ww][Aa][Rr][Nn]" { $LogFile "${LoggingProperties}["WARN_LOG_FILE"]" }
+        "[Ii][Nn][Ff][Oo]" { $LogFile = "${LoggingProperties}["INFO_LOG_FILE"]" }
+        "[Aa][Uu][Dd][Ii][Tt]" { $LogFile =" ${LoggingProperties}["AUDIT_LOG_FILE"]" }
+        "[Dd][Ee][Bb][Uu][Gg]" { $LogFile = "${LoggingProperties}["DEBUG_LOG_FILE"]" }
+        "[Mm][Oo][Nn][Ii][Tt][Oo][Rr]" { $LogFile = "${LoggingProperties}["MONITOR_LOG_FILE"]" }
+        default { $LogFile = "${LoggingProperties}["DEFAULT_LOG_FILE"]" }
     }
 
+    $LogEntry = [string]::Format("${LoggingProperties}["CONVERSION_PATTERN"]", "$(Get-Date -Format "${LoggingProperties}["TIMESTAMP_OPTS"]")", `
+        "${LogFile}", ${LogLevel}", "${ProcessID}", `
+        "${ClassName}", "${LineNumber}", "${FunctionName}", "${LogMessage}")
 
-
-
-        $CONVERSION_PATTERN = "${LoggingProperties}["CONVERSION_PATTERN"]"
-        $TIMESTAMP_OPTS = "${LoggingProperties}["TIMESTAMP_OPTS"]"
-        $PERF_LOG_FILE = "${LoggingProperties}["PERF_LOG_FILE"]"
-        $ERROR_LOG_FILE = "${LoggingProperties}["ERROR_LOG_FILE"]"
-        $DEBUG_LOG_FILE = "${LoggingProperties}["DEBUG_LOG_FILE"]"
-        $AUDIT_LOG_FILE = "${LoggingProperties}["AUDIT_LOG_FILE = "${LoggingProperties}["LOG_ROOT"]""]"
-        $WARN_LOG_FILE = "${LoggingProperties}["WARN_LOG_FILE"]"
-        $INFO_LOG_FILE = "${LoggingProperties}["INFO_LOG_FILE"]"
-        $FATAL_LOG_FILE = "${LoggingProperties}["FATAL_LOG_FILE"]"
-        $MONITOR_LOG_FILE = "${LoggingProperties}["MONITOR_LOG_FILE"]"
-        $DEFAULT_LOG_FILE = "${LoggingProperties}["DEFAULT_LOG_FILE"]"
-    $Timestamp = Get-Date -Format "${TIMESTAMP_OPTS}"
-    [string]::Format("${CONVERSION_PATTERN}",$name)
-    $LogEntry = "$Timestamp - $Message"
-    Add-Content -Path $LogPath -Value $LogEntry
+    Add-Content -Path "${LogRoot}/${LogFile}" -Value "${LogEntry}"
 }
