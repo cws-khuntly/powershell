fe1673c09152c9d401264140a6c73ae82b6e96f1
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2025-06-04 06:58:39 -0400

    Update SetupNewSystem.ps1

--- lib/setup/SetupNewSystem.ps1
+++ lib/setup/SetupNewSystem.ps1
@@ -19,7 +19,7 @@
 #>
 
 Function Build-New-System() {
-    Write-Output "Installing WSL with Fedora 42: $(wsl --install -d FedoraLinux-42; wsl --export FedoraLinux-42 C:\Temp\FedoraLinux-42.tar; wsl --unregister FedoraLinux-42; wsl --import FedoraLinux-42 C:\Temp\FedoraLinux-42.tar D:\WSL\FedoraLinux-42; Remove-Item -Path C:\Temp\FedoraLinux-42.tar)"
+    Write-Output "Installing WSL woth Fedora 42: $(Install-WSL; Write-Output "Return code: $LastExitCode")"
 
     Write-Output "Installing Chocolatey: $(Install-Chocolatey; Write-Output "Return code: $LastExitCode")"
 
